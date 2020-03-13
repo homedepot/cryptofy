9a26d8c06627297947d54a0ae2ba3ae589139b59
Mike Phillipson <MICHAEL_PHILLIPSON1@homedepot.com>
2020-03-12 19:49:44 -0500

    Added 'long_description_content_type' to setup.py for PyPi upload

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as r_file:
     readme = r_file.read()
 
 setup(name='cryptofy',
-      version='1.0.1',
+      version='1.0.2',
       description='Simple encryption/decryption functions for Python',
       long_description=readme,
+      long_description_content_type='text/markdown',
       license='Apache 2.0',
       url='https://github.com/homedepot/cryptofy',
       author='Mike Phillipson',
